0a7c705fcbc9543872a0a93263189ad7783750fb
FrancescoZinnato <147512111+FrancescoZinnato@users.noreply.github.com>
2025-02-27 11:45:40 +0100

    Test methods order to avoid errors

--- src/test/java/guru/springframework/spring6reactive/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6reactive/controllers/BeerControllerTest.java
@@ -2,13 +2,17 @@ package guru.springframework.spring6reactive.controllers;
 
 import guru.springframework.spring6reactive.model.BeerDTO;
 import guru.springframework.spring6reactive.repositories.BeerRepositoryTest;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
 @AutoConfigureWebTestClient
 class BeerControllerTest {
@@ -17,6 +21,7 @@ class BeerControllerTest {
     WebTestClient webTestClient;
 
     @Test
+    @Order(999)
     void testDeleteBeer() {
         webTestClient.delete().uri(BeerController.BEER_PATH_ID, 1)
                 .exchange()
@@ -24,6 +29,7 @@ class BeerControllerTest {
     }
 
     @Test
+    @Order(3)
     void testUpdateBeer() {
         webTestClient.put().uri(BeerController.BEER_PATH_ID, 1)
                 .body(Mono.just(BeerRepositoryTest.getTestBeer()), BeerDTO.class)
@@ -32,6 +38,7 @@ class BeerControllerTest {
     }
 
     @Test
+    @Order(4)
     void testCreateBeer() {
         webTestClient.post().uri(BeerController.BEER_PATH)
                 .body(Mono.just(BeerRepositoryTest.getTestBeer()), BeerDTO.class)
@@ -42,6 +49,7 @@ class BeerControllerTest {
     }
 
     @Test
+    @Order(1)
     void testGetById() {
         webTestClient.get().uri(BeerController.BEER_PATH_ID, 1)
                 .exchange()
@@ -51,6 +59,7 @@ class BeerControllerTest {
     }
 
     @Test
+    @Order(2)
     void testListBeers() {
         webTestClient.get().uri(BeerController.BEER_PATH)
                 .exchange()
